06a0a050800e48f9b978fd882fce19f1714671aa
carlosilva.dev <carlosilva.dev@gmail.com>
2023-03-10 15:46:19 -0300

    Correção em teste de extrato para fee_transfer

--- spec/models/bank/extract_spec.rb
+++ spec/models/bank/extract_spec.rb
@@ -25,30 +25,28 @@ RSpec.describe Bank::Model::Client, type: :model do
 
     describe 'validate_extract:' do
       describe 'extract deposit'  do
-        context 'depositing name é nulo' do
+        context 'depositing_name é nulo' do
           it 'não deixa salvar' do
-            byebug
             extract_deposit = build(:new_extract_deposit, depositing_name: nil, account: @accounts.first )
             expect(extract_deposit.save).to be_falsey
           end
         end
 
-        context 'depositing name tamanho invalido' do
+        context 'depositing_name tamanho invalido' do
           it 'não deixa salvar' do
-            byebug
             extract_deposit = build(:new_extract_deposit, depositing_name: 'ca', account: @accounts.first )
             expect(extract_deposit.save).to be_falsey
           end
         end
 
-        context 'depositing cpf é nulo' do
+        context 'depositing_cpf é nulo' do
           it 'não deixa salvar' do
             extract_deposit = build(:new_extract_deposit, depositing_cpf: nil)
             expect(extract_deposit.save).to be_falsey
           end
         end
 
-        context 'depositing cpf tamanho invalido' do
+        context 'depositing_cpf tamanho invalido' do
           it 'não deixa salvar' do
             extract_deposit = build(:new_extract_deposit, depositing_cpf: '1234')
             expect(extract_deposit.save).to be_falsey
@@ -57,33 +55,41 @@ RSpec.describe Bank::Model::Client, type: :model do
       end
 
       describe 'extract transfer sent' do
-        context 'transfer sent agency é nulo' do
+        context 'acc_transfer_agency é nulo' do
           it 'não deixa salvar' do
-            extract_transfer = build(:new_extract_transfer_sent, acc_transfer_agency: nil, account: @accounts.second )
-            expect(extract_transfer.save).to be_falsey
+            extract_transfer_sent = build(:new_extract_transfer_sent, acc_transfer_agency: nil, account: @accounts.second )
+            expect(extract_transfer_sent.save).to be_falsey
           end
         end
 
-        context 'transfer sent agency tamanho inválido' do
+        context 'acc_transfer_agency tamanho inválido' do
           it 'não deixa salvar' do
-            extract_transfer = build(:new_extract_transfer_sent, acc_transfer_agency: 12, account: @accounts.second )
-            expect(extract_transfer.save).to be_falsey
+            extract_transfer_sent = build(:new_extract_transfer_sent, acc_transfer_agency: 12, account: @accounts.second )
+            expect(extract_transfer_sent.save).to be_falsey
           end
         end
 
-        context 'transfer sent number é nulo' do
+        context 'acc_transfer_number é nulo' do
           it 'não deixa salvar' do
-            extract_transfer = build(:new_extract_transfer_sent, acc_transfer_number: nil)
-            expect(extract_transfer.save).to be_falsey
+            extract_transfer_sent = build(:new_extract_transfer_sent, acc_transfer_number: nil)
+            expect(extract_transfer_sent.save).to be_falsey
           end
         end
 
-        context 'transfer sent number tamanho invalido' do
+        context 'acc_transfer_number tamanho invalido' do
           it 'não deixa salvar' do
-            extract_transfer = build(:new_extract_transfer_sent, acc_transfer_number: 1234)
-            expect(extract_transfer.save).to be_falsey
+            extract_transfer_sent = build(:new_extract_transfer_sent, acc_transfer_number: 1234)
+            expect(extract_transfer_sent.save).to be_falsey
           end
         end
+
+        context 'fee_transfer menor ou igual a 0' do
+          it 'não deixa salvar' do
+            extract_transfer_sent = build(:new_extract_transfer_sent, fee_transfer: 0)
+            expect(extract_transfer_sent.save).to be_falsey
+          end
+        end
+
       end
     end
 
