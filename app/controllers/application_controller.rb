43151a4b4369abd6474df1d55e127d29fbb09265
carlosilva.dev <carlosilva.dev@gmail.com>
2023-03-10 01:01:54 -0300

    Adicionado recordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  def record_not_found(error)
+    render json: { error: error.message }, status: :not_found
+  end
   def format_error(context, object_name=nil)
     if context.message.present?
       { message: context.message }
